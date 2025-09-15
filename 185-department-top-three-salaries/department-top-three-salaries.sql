bce7caaabfd1d00952a042a025a451dba0ac6190
Nipunjain17 <nipunjain504@gmail.com>
2025-09-15 22:48:33 +0530

    Delete 185-department-top-three-salaries directory

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++
@@ -1,8 +0,0 @@
-# Write your MySQL query statement below
-
-SELECT d.name Department, e.name Employee, e.salary Salary
-FROM Department AS d, Employee AS e
-WHERE e.departmentId = d.id AND 
-(SELECT COUNT(DISTINCT salary) FROM Employee
-WHERE salary>e.salary AND departmentId = e.departmentId)<3
-ORDER BY d.name , e.salary DESC;
