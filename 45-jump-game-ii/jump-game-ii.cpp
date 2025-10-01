463897de278dc03d95cd40140e42b84a887d1665
Nipunjain17 <nipunjain504@gmail.com>
2025-10-01 21:55:53 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (36.97%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int jump(vector<int>& arr) {
+         int n = arr.size();
+        
+        if(n <= 1){
+            return 0;
+        }
+        if(arr[0] == 0){
+            return -1;
+        }
+        
+        int jump = 0;
+        int maxReach = 0;
+        int currReach = 0;
+        
+        for(int i=0;i<n;i++){
+            maxReach = max(maxReach,i+arr[i]);
+            
+            if(maxReach >= n-1){
+                return jump+1;
+            }
+            
+            if(i == currReach){
+                
+                if(maxReach == i){
+                    return -1;
+                }
+                
+                else{
+                    jump++;
+                    currReach = maxReach;
+                }
+            }
+        }
+        return -1;
+    }
+};
