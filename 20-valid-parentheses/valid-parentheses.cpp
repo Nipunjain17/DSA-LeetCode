5e1bbf2c3516854702ce85705c4167ddf0174136
Nipunjain17 <nipunjain504@gmail.com>
2026-01-19 21:31:51 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (86.94%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+        for(auto ch:s){
+            if(ch == '{' || ch == '[' || ch == '('){
+                stack.push(ch);
+            }
+            else{
+                if(stack.empty()) return false;
+                char top = stack.top();
+                stack.pop();
+
+                if((ch == ')' && top != '(' ) || 
+                (ch == '}' && top != '{' ) ||
+                 (ch == ']' && top != '[') ){
+                    return false;
+                 }
+            }
+        }
+        return stack.empty();        
+    }
+};
